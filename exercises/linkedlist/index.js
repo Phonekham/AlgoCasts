12e33d9a74ce8a47ea6860acb80ec24346f3b80a
Phonekham <phone.madridista@gmail.com>
2019-09-20 10:18:44 +0700

    insert first

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,26 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+}
+
+const nodeOne = new Node(5);
+const list = new LinkedList();
+list.head = nodeOne;
+list.insertFirst(15);
+list;
 
 module.exports = { Node, LinkedList };
